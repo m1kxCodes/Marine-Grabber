5abe6084e9ef4692728f652acf13df14ae91b524
m1kxCodes <153386657+m1kxCodes@users.noreply.github.com>
2024-11-16 16:59:12 -0500

    Update setup.bat

--- MGrabber/setup.bat
+++ MGrabber/setup.bat
@@ -1,10 +1,8 @@
 @echo off
 title Marine Grabber Setup
 
-:: Display a message indicating that the dependencies are being installed
 echo Installing dependencies for the grabber...
 
-:: Function to check and install dependencies
 call :install_dependency requests
 call :install_dependency pyautogui
 call :install_dependency pynput
@@ -17,13 +15,10 @@ call :install_dependency win32api
 
 echo Dependencies installed successfully!
 
-:: Pause to see the message before closing
 pause
 goto :eof
 
-:: Function to install a dependency and check if it was installed
 :install_dependency
-:: Check if the dependency is already installed
 pip show %1 >nul 2>&1
 if %errorlevel%==0 (
     echo Already have %1
